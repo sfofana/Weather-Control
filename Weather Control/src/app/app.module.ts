51f01fd984dad05a0b4427b9b072e1fccd6f879a
sfofana <sufyanfofana@yahoo.com>
2020-02-25 19:10:17 -0600

    update with api

--- Weather Control/src/app/app.module.ts
+++ Weather Control/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { HttpClientModule } from '@angular/common/http';
+import { MDBBootstrapModule, WavesModule, ButtonsModule, CardsModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,12 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     MDBBootstrapModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    WavesModule,
+    ButtonsModule,
+    CardsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
